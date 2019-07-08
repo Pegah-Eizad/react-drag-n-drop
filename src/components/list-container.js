5a125fe4d9df6f91a2c25eb687a03fc9aa99e610
Pegah Eizadkhah <peizadkh@gmail.com>
2019-07-08 13:37:06 -0400

    sets up some of the basic files

---
+++ src/components/list-container.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './list-container.css';
+
+function ListContainer() {
+    return (
+      <div className="list-container">
+
+      </div>
+    );
+  }
+  
+  export default ListContainer;
